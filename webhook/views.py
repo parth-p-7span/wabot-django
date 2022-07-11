d1ce0e83b76907a137b45cc3bc53426950fe8cba
hitchhicker8401@hotmail.com <parth.p@7span.com>
2022-07-11 18:26:58 +0530

    new commit

--- webhook/views.py
+++ webhook/views.py
@@ -6,14 +6,14 @@ from django.conf import settings
 from django.db.transaction import atomic, non_atomic_requests
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_http_methods
 from django.utils import timezone
 
 from .models import WebhookMessage
 
 
 @csrf_exempt
-@require_POST
+@require_http_methods(['GET', 'POST'])
 @non_atomic_requests
 def wa_webhook(request):
     print("====> ", request.content_params)
